eadd72365b47201c1c1f0208539b8c3a6fb38eb3
ahmed <ahmedesmaelgamal@gmail.com>
2025-11-23 23:07:35 +0200

    update

--- resources/views/emails/meetings/new_meeting.blade.php
+++
@@ -1,21 +0,0 @@
-@component('mail::message')
-# إشعار اجتماع جديد 📅
-
-مرحبًا {{ $owner->name }}،
-
-تم إنشاء اجتماع جديد للجمعية الخاصة بك.
-
-**تاريخ الاجتماع:** {{ \Carbon\Carbon::parse($meeting->date)->format('Y-m-d H:i') }}  
-**العنوان:** {{ $meeting->address }}  
-
-@if($meeting->agenda)
-**جدول الأعمال:** {{ $meeting->agenda->title }}
-@endif
-
-@component('mail::button', ['url' => url('/')])
-عرض التفاصيل
-@endcomponent
-
-شكرًا لتعاونك،  
-{{ config('app.name') }}
-@endcomponent
